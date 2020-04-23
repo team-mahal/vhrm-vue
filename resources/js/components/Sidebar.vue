7bdef076576b1ad33feda40c91d74d9566a99ec0
jual ahmed <md.jual.ah@gmail.com>
2020-04-23 11:00:45 +0600

    make logo perfect

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -33,73 +33,76 @@
         </ul>
     </div>
     
-    <div v-else id="sidebar" class="h-screen w-64 menu bg-white text-white px-0 flex nunito static fixed shadow">
+     <div v-else id="sidebar" class="h-screen w-64 menu bg-white text-white flex nunito static fixed shadow">
         <ul class="list-reset w-full">
-            <li class="my-2 md:my-0">
-                <router-link to="/district" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block active">Districts</span>
+            <li class="relative">
+               <div class="absolute z-20 posation cursor-pointer" @click="taggleview()">
+                    <svg aria-hidden="true" data-prefix="fad" data-icon="align-justify" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-8 w-8 pl-2 text-green-700"><g class="fa-group">
+                        <path fill="currentColor" d="M432 416H16a16 16 0 00-16 16v32a16 16 0 0016 16h416a16 16 0 0016-16v-32a16 16 0 00-16-16zm0-256H16a16 16 0 00-16 16v32a16 16 0 0016 16h416a16 16 0 0016-16v-32a16 16 0 00-16-16z" class="fa-secondary"/>
+                        <path fill="currentColor" d="M432 288H16a16 16 0 00-16 16v32a16 16 0 0016 16h416a16 16 0 0016-16v-32a16 16 0 00-16-16zm0-256H16A16 16 0 000 48v32a16 16 0 0016 16h416a16 16 0 0016-16V48a16 16 0 00-16-16z" class="fa-primary"/></g>
+                    </svg>
+               </div>
+            </li>
+            <li class="text-center text-green-900 py-1">
+                <router-link to="/#">
+                    <h2 class="text-sm p-2 text-4xl text-center mt-1">
+                        <img src="/logo.png" alt="" class="w-10 mx-auto">
+                    </h2>
                 </router-link>
             </li>
-            <li class="my-2 md:my-0">
-                <router-link to="/upazila" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block active">Upazilas</span>
+            <li class="my-2 md:my-0">   
+                <router-link to="/#" class="py-2 pl-1 text-gray-600 no-underline hover:text-green-200 flex items-center">
+                    <svg aria-hidden="true" data-prefix="fad" data-icon="house-damage" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-4 h-4">
+                        <g class="fa-group">
+                            <path fill="currentColor" d="M512 311.08V496a16 16 0 01-16 16H319.82l-39.92-55.26 104.11-64L236 256l60.11 119.2L192 439.18 229.24 512H80a16 16 0 01-16-16V311c1.78-1.21 3.85-1.89 5.47-3.35L288 115l218.74 192.9c1.55 1.36 3.57 2.02 5.26 3.18z" class="fa-secondary"/>
+                            <path fill="currentColor" d="M236 256l60.11 119.2L192 439.18 229.24 512h90.58l-39.92-55.26 104.11-64zm334.7-19.74L512 184.43V48a16 16 0 00-16-16h-64a16 16 0 00-16 16v51.67L314.76 10.29a39.85 39.85 0 00-53.45 0l-256 226a16 16 0 00-1.21 22.59l21.41 23.79a16 16 0 0022.59 1.21L277.43 81.61a16 16 0 0121.17 0l229.32 202.27a16 16 0 0022.6-1.2l21.4-23.82a16 16 0 00-1.22-22.6z" class="w-1 h-1"/>
+                        </g>
+                    </svg>
+                    <span class="w-full inline-block pb-1 md:pb-0 text-sm pl-2 hidden sm:hidden md:block lg:block xl:block">Home</span>
                 </router-link>
             </li>
-            <li class="my-2 md:my-0">
-                <router-link to="/client" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block active">Clients</span>
+
+            <li class="my-2 md:my-0" v-for="(item,index) in navitem">
+                <router-link v-if="item.link" :to="item.link" class="py-2 pl-1 text-gray-600 no-underline hover:text-green-200 flex items-center">
+                    <span v-html="item.icon"></span>
+                    <span class="w-full inline-block pb-1 md:pb-0 text-sm pl-2 hidden sm:hidden md:block lg:block xl:block">{{ item.name }}</span>
                 </router-link>
+                <div v-else class="py-2 pl-1 text-gray-600 no-underline hover:text-green-200 flex items-center cursor-pointer" @click="toggle(index)">
+                    <span v-html="item.icon"></span>
+
+                    <span class="w-full inline-block pb-1 md:pb-0 text-sm pl-2 hidden sm:hidden md:block lg:block xl:block">{{ item.name }}</span>
+
+                    <svg class="w-4 h-4" v-if="!item.collapsed" aria-hidden="true" data-prefix="fas" data-icon="angle-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/></svg>
+
+                    <svg class="w-4 h-4" v-else aria-hidden="true" data-prefix="fas" data-icon="angle-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" ><path fill="currentColor" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"/></svg>
+                </div>
+                <ul class="list-reset w-full block" v-if="item.child && item.collapsed">
+                    <li class="my-2 md:my-0" v-for="(i,index1) in item.child"> 
+
+                        <router-link v-if="!i.child" :to="i.link" class="py-2 pl-1 text-gray-600 no-underline hover:text-green-200 flex items-center">
+                            <span class="w-full inline-block pb-1 md:pb-0 text-sm pl-2 hidden sm:hidden md:block lg:block xl:block pl-10">{{ i.name }}</span>
+                        </router-link>
+
+                        <div v-else class="ml-2 py-2 pl-1 text-gray-600 no-underline hover:text-green-200 flex items-center cursor-pointer" @click="toggles(index,index1)">
+                            <span v-html="i.icon"></span>
+
+                            <span class="w-full inline-block pb-1 md:pb-0 text-sm pl-2 hidden sm:hidden md:block lg:block xl:block">{{ i.name }}</span>
+
+                            <svg class="w-4 h-4" v-if="!i.collapsed" aria-hidden="true" data-prefix="fas" data-icon="angle-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/></svg>
+
+                            <svg class="w-4 h-4" v-else aria-hidden="true" data-prefix="fas" data-icon="angle-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" ><path fill="currentColor" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"/></svg>
+                        </div>
+                        <ul class="list-reset w-full block" >
+                            <li class="my-2 md:my-0" v-for="j in i.child" v-if="i.child && i.collapsed"> 
+                                <router-link :to="j.link" class="pl-3 py-2 text-gray-600 no-underline hover:text-green-200 flex items-center">
+                                    <span class="w-full inline-block pb-1 md:pb-0 text-sm pl-2 hidden sm:hidden md:block lg:block xl:block pl-10">{{ j.name }}</span>
+                                </router-link>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </li>
-            <li class="my-2 md:my-0">
-                <router-link to="/project" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Projects</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="/department" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Departments</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="designation" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Designations</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="status" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Statuses</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="issue-type" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Issue Types</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="issue" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Issue</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="issues" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Issues</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="taskcurd" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Task</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="task" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Tasks</span>
-                </router-link>
-            </li>
-            <li class="my-2 md:my-0">
-                <router-link to="holidays" class="block py-2 justify-center md:md:justify-start pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                    <span class="w-full inline-block pb-1 md:pb-0 text-sm hidden sm:hidden md:block lg:block xl:block">Holidays</span>
-                </router-link>
-            </li>
+
         </ul>
     </div>
 </div>
