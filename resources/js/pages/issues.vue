1e18d13b1b47970983eb19e4fdc7f9192b536e64
jual ahmed <md.jual.ah@gmail.com>
2020-04-29 12:22:08 +0600

    solve status issue

--- resources/js/pages/issues.vue
+++ resources/js/pages/issues.vue
@@ -150,11 +150,13 @@
 		    <svg class="fill-current h-4 w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 		    	<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
 		    </svg>
-		    	<span v-if="singleissue.status">{{ singleissue.status.name }}</span>
+		   	<span>{{ singleissue.status }}</span>
 		    <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
-		      <li class="" v-for="st in status" @click="statusupdate(st.id)">
-		      	<a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">{{ st.name }}</a>
-		      </li>
+		      	<li>
+		      		<a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" @click="statusupdate('Backlog')" href="#">Backlog</a>
+		      		<a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" @click="statusupdate('Picket')" href="#">Picket</a>
+		      		<a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" @click="statusupdate('Done')" href="#">Done</a>
+		      	</li>
 		    </ul>
 		  </div>
 		</h2>
@@ -269,13 +271,13 @@ export default {
 
 	  			this.issues.find((item)=>{
 	  				if(item.id==this.singleissue.id){
-	  					item.status_id=id
+	  					item.status=id
 	  				}
 	  			})
 
 	  			let { data } = await axios.post('api/issues/statusupdate',{
 													    issue_id: this.singleissue.id,
-													    status_id: id,
+													    status: id,
 													})
 	  			this.singleissue=data;
 	  		}
